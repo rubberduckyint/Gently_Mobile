39bad4a912d18c7c81f66eba4fcbf76439b7d289
Luke Ollett <ollett@gmail.com>
2025-09-30 12:38:55 -0700

    Update app.config.ts

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "Gently",
   slug: "gently",
   scheme: "gently",
-  version: "0.1.1",
+  version: "0.1.2",
   orientation: "portrait",
   icon: "./assets/gently-ios-blue.png",
   userInterfaceStyle: "automatic",
